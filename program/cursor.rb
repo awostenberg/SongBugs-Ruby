90d2c2766ea678556db0b0540eadb6591a16011d
J. Wostenberg <pwostenberg@yahoo.com>
2011-12-20 18:37:51 -0700

    Oops, I didn't define Cursor in the SongBugs namespace

--- program/cursor.rb
+++ program/cursor.rb
@@ -1,21 +1,23 @@
-class Cursor
-  def initialize(window)
-    @window = window
-  end
+module SongBugs
+  class Cursor
+    def initialize(window)
+      @window = window
+    end
 
-  def state
-    Rubydraw.info
-  end
+    def state
+      Rubydraw.info
+    end
 
-  def position
-    Rubydraw.position
-  end
+    def position
+      Rubydraw.position
+    end
 
-  # Returns true if the mouse pointer is inside +rect,+ and
-  # false otherwise.
-  def inside?(rect)
-    position.inside?(rect)
-  end
+    # Returns true if the mouse pointer is inside +rect,+ and
+    # false otherwise.
+    def inside?(rect)
+      position.inside?(rect)
+    end
 
-  alias touching? inside?
+    alias touching? inside?
+  end
 end
